953ce461a7e37236893b46d3f36a612f504b9cad
SkProGL <gl.is.watching@gmail.com>
2026-03-16 20:57:29 +0000

    minor changes

--- start.ps1
+++ start.ps1
@@ -1,3 +1,4 @@
 python start.py
-rclone copy results gdrive:results
+C:\Users\g2-leonovs\repo\remote-main\rclone\rclone.exe config
+C:\Users\g2-leonovs\repo\remote-main\rclone\rclone.exe copy results gdrive:results
 shutdown /l
