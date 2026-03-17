24eb5759a1797cad7042130b61d620bea4bb5a11
SkProGL <gl.is.watching@gmail.com>
2026-03-17 21:29:51 +0000

    fixed pip install

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,2 @@
- uv pip install -r requirements.txt --system
+ uv pip install -r requirements.txt
  uv pip install --pre torch torchvision --index-url "https://download.pytorch.org/whl/nightly/cu128"
