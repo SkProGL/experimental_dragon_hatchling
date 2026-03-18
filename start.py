45c7052a634d2ce7c0176cf1ca4191fc1da047e9
SkProGL <gl.is.watching@gmail.com>
2026-03-18 21:23:18 +0000

    BDH startup

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
-# from factual_models.bdh_train import main as bfm
-from factual_models.tf_train import main as tfm
+from factual_models.bdh_train import main as bfm
+# from factual_models.tf_train import main as tfm
 
 if __name__ == "__main__":
-    tfm()
+    # tfm()
+    bfm()
